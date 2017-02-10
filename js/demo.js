193a955d4002cdd614264cd5d6e40f2e31d8c83e
Regi24 <mrregi24@gmail.com>
2017-02-10 20:55:15 +1100

    Set options via constructor

--- js/demo.js
+++ js/demo.js
@@ -1,27 +1,16 @@
-/**
- * Particleground demo
- * @author Jonathan Nicol - @mrjnicol
- */
-
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
+    //dotColor: '#5cbdaa',
+    //lineColor: '#5cbdaa'
+	    dotColor: '#377486',
+        lineColor: '#377486',
+        particleRadius: 3,
+        parallaxMultiplier: 20,
+        proximity: 170,
+        density: 15000
+
+	
   });
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
 }, false);
-
-
-/*
-// jQuery plugin example:
-$(document).ready(function() {
-  $('#particles').particleground({
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
-  });
-  $('.intro').css({
-    'margin-top': -($('.intro').height() / 2)
-  });
-});
-*/
